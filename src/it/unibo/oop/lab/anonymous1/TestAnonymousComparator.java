a1b555956513722fc3ffc8154f00c6d2a4183276
Domenico Giacobbi <domenico.giacobbi@studio.unibo.it>
2021-11-13 17:50:48 +0100

    definition of an anonymous comparator

--- src/it/unibo/oop/lab/anonymous1/TestAnonymousComparator.java
+++ src/it/unibo/oop/lab/anonymous1/TestAnonymousComparator.java
@@ -1,6 +1,8 @@
 package it.unibo.oop.lab.anonymous1;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import it.unibo.oop.lab.socialnetwork.SocialNetworkUser;
@@ -74,7 +76,13 @@ public final class TestAnonymousComparator {
          * 
          * REFER TO LESSON 13-Advanced-Mechanisms.pdf, slide 41
          */
-        // TODO
+        
+        final Comparator<User> comp = new Comparator<User>() {
+        	public int compare(final User usr1,final User usr2) {
+        		return usr1.getAge() - usr2.getAge();
+        	}
+        };
+        
         /*
          * expected Result
          */
