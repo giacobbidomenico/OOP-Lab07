75bb8932dfac9a7b2e5bb5c07c096ff81cca8ae7
Domenico Giacobbi <domenico.giacobbi@studio.unibo.it>
2021-11-13 18:20:23 +0100

    implementation of the method hasSport

--- src/it/unibo/oop/lab/enum1/SportSocialNetworkUserImpl.java
+++ src/it/unibo/oop/lab/enum1/SportSocialNetworkUserImpl.java
@@ -94,6 +94,6 @@ public class SportSocialNetworkUserImpl<U extends User> extends SocialNetworkUse
      * @return true if the user likes sport s
      */
     public boolean hasSport(final Sport s) {
-        return false;
+        return this.sportsUser.contains(s);
     }
 }
