08915b20ac01abc43917b59b841bb15dacd8179d
Domenico Giacobbi <domenico.giacobbi@studio.unibo.it>
2021-11-14 11:04:04 +0100

    create enumeration Place

---
+++ src/it/unibo/oop/lab/enum2/Place.java
@@ -0,0 +1,18 @@
+package it.unibo.oop.lab.enum2;
+
+/*
+ * Enumeration describing the place where a particular sport takes place
+ */
+
+public enum Place {
+	
+	/*
+	 * Indoor
+	 */
+	INDOOR,
+	
+	/*
+	 * Outdoor
+	 */
+	OUTDOOR;
+}
