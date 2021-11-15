7c2d3757bbb4bb9a6957e0c4063ad79073e5fd30
Domenico Giacobbi <domenico.giacobbi@studio.unibo.it>
2021-11-15 12:16:38 +0100

    add fields of the class

--- src/it/unibo/oop/lab/nesting2/OnListAcceptable.java
+++ src/it/unibo/oop/lab/nesting2/OnListAcceptable.java
@@ -1,7 +1,10 @@
 package it.unibo.oop.lab.nesting2;
 
-public class OnListAcceptable<T> implements Acceptable {
+import java.util.List;
 
+public class OnListAcceptable<T> implements Acceptable {
+	
+	private final List<T> list;
 	
 	@Override
 	public Acceptor<T> acceptor() {
