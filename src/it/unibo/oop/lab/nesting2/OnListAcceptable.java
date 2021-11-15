5632823c416b5ab26c7589727d30dc7fc3bc1898
Domenico Giacobbi <domenico.giacobbi@studio.unibo.it>
2021-11-15 12:20:08 +0100

    define the constructor of the class

--- src/it/unibo/oop/lab/nesting2/OnListAcceptable.java
+++ src/it/unibo/oop/lab/nesting2/OnListAcceptable.java
@@ -2,13 +2,16 @@ package it.unibo.oop.lab.nesting2;
 
 import java.util.List;
 
-public class OnListAcceptable<T> implements Acceptable {
+public class OnListAcceptable<T> implements Acceptable<T> {
 	
 	private final List<T> list;
+	
+	public OnListAcceptable(final List<T> list){
+		this.list = list;
+	}
 	
 	@Override
 	public Acceptor<T> acceptor() {
 		return null;
 	}
-
 }
