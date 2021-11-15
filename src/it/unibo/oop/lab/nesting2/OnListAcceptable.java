c680fc090318381d187d4fb877042aec774cc3ba
Domenico Giacobbi <domenico.giacobbi@studio.unibo.it>
2021-11-15 16:43:01 +0100

    implementation of the method acceptor

--- src/it/unibo/oop/lab/nesting2/OnListAcceptable.java
+++ src/it/unibo/oop/lab/nesting2/OnListAcceptable.java
@@ -1,5 +1,6 @@
 package it.unibo.oop.lab.nesting2;
 
+import java.util.Iterator;
 import java.util.List;
 
 public class OnListAcceptable<T> implements Acceptable<T> {
@@ -12,6 +13,27 @@ public class OnListAcceptable<T> implements Acceptable<T> {
 	
 	@Override
 	public Acceptor<T> acceptor() {
-		return null;
+		return new Acceptor<T>() {
+			private final Iterator<T> iter = list.iterator();
+			
+			@Override
+			public void accept(T newElement) throws ElementNotAcceptedException {
+				
+				if((!iter.hasNext()) || (!newElement.equals(iter.next()))) {
+					throw new ElementNotAcceptedException(newElement);
+				}
+				
+			}
+
+			@Override
+			public void end() throws EndNotAcceptedException {
+				
+				if(iter.hasNext()) {
+					throw new EndNotAcceptedException();
+				}
+				
+			}
+			
+		};
 	}
 }
