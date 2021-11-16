b7d299af40cea784a909b9b3f26af1e09fa105ba
Domenico Giacobbi <domenico.giacobbi@studio.unibo.it>
2021-11-16 12:24:53 +0100

    add javadoc documentation of the class OnListAcceptable

--- src/it/unibo/oop/lab/nesting2/OnListAcceptable.java
+++ src/it/unibo/oop/lab/nesting2/OnListAcceptable.java
@@ -1,21 +1,41 @@
+/**
+ * 
+ */
 package it.unibo.oop.lab.nesting2;
 
 import java.util.Iterator;
 import java.util.List;
 
+/**
+ * Implementation of the interface Acceptable
+ * 	
+ * @param <T>
+ */
 public class OnListAcceptable<T> implements Acceptable<T> {
 	
 	private final List<T> list;
 	
+	/**
+     * Builds a new {@link OnListAcceptable}.
+     * 
+     * @param list
+     *            the managed list
+     */
 	public OnListAcceptable(final List<T> list){
 		this.list = list;
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public Acceptor<T> acceptor() {
 		return new Acceptor<T>() {
 			private final Iterator<T> iter = list.iterator();
 			
+			/**
+			 *	{@inheritDoc}
+			 */
 			@Override
 			public void accept(T newElement) throws ElementNotAcceptedException {
 				
@@ -25,6 +45,9 @@ public class OnListAcceptable<T> implements Acceptable<T> {
 				
 			}
 
+			/**
+			 * {@inheritDoc}
+			 */
 			@Override
 			public void end() throws EndNotAcceptedException {
 				
